aaba746d57df87581461876c9ca7d835a51220a1
Халтурин Никита <nikita.khalturin@gazprombank.ru>
2026-01-05 16:51:36 +0500

    Выполнение задач второго спринта

--- src/microservices/proxy/src/main/kotlin/ru/svn/config/StranglerProperties.kt
+++ src/microservices/proxy/src/main/kotlin/ru/svn/config/StranglerProperties.kt
@@ -7,7 +7,7 @@ import java.net.URI
 @Configuration
 @ConfigurationProperties(prefix = "cinemaabyss.strangler")
 data class StranglerProperties(
-    var gradualMigration: Boolean = false,
+    var gradualMigration: Boolean = true,
     var moviesMigrationPercent: Double = 0.0,
     var monolithUrl: URI = URI.create("http://localhost:8080"),
     var moviesServiceUrl: URI = URI.create("http://localhost:8081"),
